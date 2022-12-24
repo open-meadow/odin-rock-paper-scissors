4900fd763a9a5dcd65e72062cdee69f725f71eb9
open-meadow <rohan.varughese@protonmail.com>
2022-12-24 18:59:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+// find computer input
+  // use Math.random between 0, 1, 2
+  // 0 = rock
+  // 1 = paper
+  // 2 = scissors
+// take user input
+// compare
+// show winner
